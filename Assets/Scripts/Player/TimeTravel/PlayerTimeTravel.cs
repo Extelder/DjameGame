7609fa90fe7549b7188541cba0d52e6d43fc82ea
Extelder <Nikolaoskas15@gmail.com>
2025-01-10 20:44:58 +0700

    Added music

--- Assets/Scripts/Player/TimeTravel/PlayerTimeTravel.cs
+++ Assets/Scripts/Player/TimeTravel/PlayerTimeTravel.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using EvolveGames;
 using UnityEngine;
 
 public class PlayerTimeTravel : MonoBehaviour
 {
+    [SerializeField] private Transform _player;
+    [SerializeField] private PlayerController _controller;
+    [SerializeField] private Transform _travelPoint;
+
     [SerializeField] private KeyCode _travelKey;
 
     private bool _travelling;
@@ -16,16 +21,21 @@ public class PlayerTimeTravel : MonoBehaviour
         if (Input.GetKeyDown(_travelKey) && !_travelling)
         {
             TravelStart?.Invoke();
+            _controller.enabled = false;
             _travelling = true;
         }
     }
 
     public void PerformTravel()
     {
+        Vector3 tpPos = _travelPoint.position;
+        _travelPoint.position = _player.position;
+        _player.position = tpPos;
     }
 
     public void TravelEnd()
     {
         _travelling = false;
+        _controller.enabled = true;
     }
 }
