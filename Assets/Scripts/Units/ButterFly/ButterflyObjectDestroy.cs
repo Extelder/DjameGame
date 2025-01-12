f9d1ac75a233ed53be53b7d06d4f135a74584da2
igoryava <126491702+igoryava@users.noreply.github.com>
2025-01-12 16:51:57 +0300

    Fixed bugs + added sound

--- Assets/Scripts/Units/ButterFly/ButterflyObjectDestroy.cs
+++ Assets/Scripts/Units/ButterFly/ButterflyObjectDestroy.cs
@@ -6,12 +6,16 @@ public class ButterflyObjectDestroy : HitBox
 {
     [SerializeField] private GameObject[] _objects;
     [SerializeField] private PlaySound _sound;
+    [SerializeField] private GameObject _hat;
+    [SerializeField] private PlayerTimeTravel _timeTravel;
     public override void Hit(Rapier rapier)
     {
         for (int i = 0; i < _objects.Length; i++)
         {
             Destroy(_objects[i]);
         }
+        _hat.SetActive(false);
+        _timeTravel.enabled = false;
         _sound.PlayAudio();
         base.Hit(rapier);
     }
